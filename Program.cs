1a4c0b1f046349b1d60c6eddce8ead9e2fcda16e
Serhii Penteliuk <sergunyapw@gmail.com>
2021-02-10 17:27:13 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace try2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string buf;
+
+            Console.WriteLine("Input alpha: ");
+            buf = Console.ReadLine();
+            double a = Convert.ToDouble(buf);
+
+            double z1 = Math.Sin((Math.PI / 2) + (3 * a)) / (1 - Math.Sin((3 * a) - Math.PI));
+            double z2 = 1 / Math.Tan((5 / 4 * Math.PI) + (3 / 2 * a));
+
+            Console.WriteLine("z1=" + z1);
+            Console.WriteLine("z2=" + z2);
+        }
+    }
+}
